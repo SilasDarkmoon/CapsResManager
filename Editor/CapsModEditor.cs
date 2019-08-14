80ef4884aad22ee0c9d0ed84330b9909cd83c6c2
Silas <hdss3609@gmail.com>
2019-08-14 16:33:50 +0800

    只去Reimport ResManager.dll

--- Editor/CapsModEditor.cs
+++ Editor/CapsModEditor.cs
@@ -266,14 +266,14 @@ namespace Capstones.UnityEditorEx
                 Array.Sort(lines);
                 System.IO.File.WriteAllLines("Assets/mcs.rsp", lines);
                 System.IO.File.WriteAllLines("Assets/csc.rsp", lines);
-                AssetDatabase.ImportAsset(CapsEditorUtils.__ASSET__, ImportAssetOptions.ForceUpdate);
+                //AssetDatabase.ImportAsset(CapsEditorUtils.__ASSET__, ImportAssetOptions.ForceUpdate);
                 AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(ScriptableObject.CreateInstance<CapsModDesc>())), ImportAssetOptions.ForceUpdate);
-                // Update all package...
-                foreach (var kvp in _PackageName2ModName)
-                {
-                    var pname = kvp.Key;
-                    AssetDatabase.ImportAsset("Packages/" + pname, ImportAssetOptions.ForceUpdate);
-                }
+                //// Update all package...
+                //foreach (var kvp in _PackageName2ModName)
+                //{
+                //    var pname = kvp.Key;
+                //    AssetDatabase.ImportAsset("Packages/" + pname, ImportAssetOptions.ForceUpdate);
+                //}
             }
             AssetDatabase.Refresh();
         }
