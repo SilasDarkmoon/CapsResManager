c2e25df090d6fa4f3af9b04b60331d0df4dd2f76
Silas <hdss3609@gmail.com>
2019-09-04 16:14:39 +0800

    ResHolder

---
+++ Runtime/CapsResHolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Object = UnityEngine.Object;
+
+namespace Capstones.UnityEngineEx
+{
+    public class CapsResHolder : MonoBehaviour
+    {
+        public List<Object> ResList;
+    }
+}
---
+++ Runtime/CapsResManager_ResHolder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Object = UnityEngine.Object;
+
+namespace Capstones.UnityEngineEx
+{
+    public static partial class ResManager
+    {
+        public static void HoldRes(this GameObject parent, Object res)
+        {
+            if (parent && res)
+            {
+                var holder = parent.GetComponent<CapsResHolder>();
+                if (!holder)
+                {
+                    holder = parent.AddComponent<CapsResHolder>();
+                }
+                if (holder.ResList == null)
+                {
+                    holder.ResList = new List<Object>();
+                }
+                holder.ResList.Add(res);
+            }
+        }
+        public static void HoldRes(this Component parent, Object res)
+        {
+            if (parent && res)
+            {
+                HoldRes(parent.gameObject, res);
+            }
+        }
+        private static CapsResHolderEx _ExHolder;
+        public static void HoldRes(this object parent, object res)
+        {
+            if (!_ExHolder)
+            {
+                var holderobj = new GameObject();
+                holderobj.hideFlags = HideFlags.HideAndDontSave;
+                Object.DontDestroyOnLoad(holderobj);
+                _ExHolder = holderobj.AddComponent<CapsResHolderEx>();
+            }
+            _ExHolder.AddHolder(parent, res);
+        }
+    }
+}
